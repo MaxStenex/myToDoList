25edfb1513b93307a66dd0590403808ae741ed6c
Maxim <yourstenex@gmail.com>
2020-08-12 22:06:41 +0300

    Added localStorage

--- #source/js/main.js
+++ #source/js/main.js
@@ -2,30 +2,67 @@ let todoWorkInput = document.querySelector('.todo-work__input');
 let todoAddButton = document.querySelector('.todo-work__add-but');
 let todoWorkList = document.querySelector('.todo-list');
 let todoSelector = document.querySelector('.todo-work__selector');
+let datas = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')) : {
+   completed: [],
+   notCompleted: [],
+};
 
 
-todoAddButton.addEventListener('click', todoAdd);
-todoSelector.addEventListener('change', todoSelectChange);
+//Rendering todoItems from localStorage
+renderTodoList();
 
 
-function todoAdd(evt) {
+todoAddButton.addEventListener('click', function (evt) {
    evt.preventDefault();
-
    //Check for input value emptiness 
-   if ((todoWorkInput.value).trim() == '') {
+   let text = todoWorkInput.value;
+   if ((text).trim() == '') {
       todoWorkInput.value = '';
       return;
    }
+   datas.notCompleted.push(text);
+   todoAdd(text);
+});
+todoSelector.addEventListener('change', todoSelectChange);
 
+
+//Adding items in localStorage
+function datasObjectUpdated() {
+   localStorage.setItem('todoList', JSON.stringify(datas));
+};
+
+function renderTodoList() {
+   if (!datas.completed.length && !datas.notCompleted.length) return;
+
+   for (let i = 0; i < datas.completed.length; i++) {
+      let text = datas.completed[i];
+      todoAdd(text, true);
+   }
+
+   for (let j = 0; j < datas.notCompleted.length; j++) {
+      let text = datas.notCompleted[j];
+      todoAdd(text);
+   }
+};
+
+//DOM manipulations with todoList item (adding, changing state , etc...)
+function todoAdd(text, completed) {
    let todoItem = document.createElement('li');
    let todoItemText = document.createElement('p');
    let todoDeleteBut = document.createElement('button');
    let todoDeleteButImg = document.createElement('img');
+   let todoCompleted = () => {
+      return todoItemText.classList.contains('todo-list__text--completed') ? true : false;
+   };
+
+   if (completed) {
+      todoItemText.classList.add('todo-list__text--completed')
+   }
    todoDeleteButImg.src = "img/delete.png";
 
    todoItem.classList.add('todo-list__item');
    todoItemText.classList.add('todo-list__text');
-   todoItemText.innerText = todoWorkInput.value;
+   todoItemText.innerText = text;
    todoDeleteBut.classList.add('todo-list__del-but');
    todoDeleteBut.appendChild(todoDeleteButImg);
 
@@ -33,16 +70,33 @@ function todoAdd(evt) {
    todoItem.appendChild(todoDeleteBut);
 
    todoDeleteBut.addEventListener('click', () => {
+      if (todoCompleted()) {
+         datas.completed.splice(datas.completed.indexOf(todoItemText.innerText), 1);
+      } else {
+         datas.notCompleted.splice(datas.notCompleted.indexOf(todoItemText.innerText), 1);
+      };
+      datasObjectUpdated();
       todoDeleteBut.parentElement.remove();
    });
+
    todoItem.addEventListener('click', (evt) => {
       if (evt.target != todoDeleteBut) {
-         todoItemText.classList.toggle('todo-list__text--completed');
+         if (todoCompleted()) {
+            todoItemText.classList.toggle('todo-list__text--completed');
+            datas.completed.splice(datas.completed.indexOf(text), 1);
+            datas.notCompleted.push(text);
+         } else {
+            todoItemText.classList.toggle('todo-list__text--completed');
+            datas.notCompleted.splice(datas.notCompleted.indexOf(text), 1);
+            datas.completed.push(text);
+         }
+         datasObjectUpdated();
       }
    });
 
    todoWorkList.appendChild(todoItem);
    todoWorkInput.value = '';
+   datasObjectUpdated();
 };
 
 function todoSelectChange(evt) {
